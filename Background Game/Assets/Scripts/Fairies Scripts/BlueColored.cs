0d594ac9121df6dd6995eaf4f043803f3bb56ee6
Perezoso08 <franciscoponcezambrano08@gmail.com>
2020-02-14 11:24:47 +0100

    aaaa

---
+++ Background Game/Assets/Scripts/Fairies Scripts/BlueColored.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueColored : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "PJ")
+        {
+            Animator animator = GetComponent<Animator>();
+            animator.SetTrigger("Blue");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Background Game/Assets/Scripts/Fairies Scripts/YellowColored.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowColored : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "PJ")
+        {
+            Animator animator = GetComponent<Animator>();
+            animator.SetTrigger("Yellow");
+            //animator.Play("Colored Yellow");
+            Destroy(gameObject);
+        }
+    }
+}
